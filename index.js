d8be33f187aa44d1e1918fcb0c55e3f6ba18dd88
Avaneesh-Ravat <avaneesh2004ravat@gmail.com>
2024-07-12 10:40:52 +0530

    home page nav created

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-let a=10;
+let port = 8080;
+//npm install express
+const express = require("express");
+const app = express();
+const path  = require("path");
+const methodOverride = require("method-override"); //npm i method-override
+
+//npm i ejs
+app.set("view engine", "ejs");
+app.use(methodOverride("_method")); 
+app.set("views", path.join(__dirname, "views")); // to access from outside of folder by path
+app.use(express.static(path.join(__dirname, "public"))); // to use html, css, js file togrther
+app.use(express.urlencoded({extended: true})); // parse post request data
+app.use(express.json()); // to handle json data
+
+
+//to render home page
+app.get("/home", (req, res)=>{
+    res.render("index.ejs");
+});
+
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port ${port}`);
+});
